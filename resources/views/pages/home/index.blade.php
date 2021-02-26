d9e1670724f89dc0a33c6d54334194fe74b0b78a
vsahinid <vsahinid@alumni.cmu.edu>
2021-02-25 18:10:41 -0600

    Seeing if jquery loads

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -12,5 +12,8 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
 
 <script>
+    $(document).ready(function() {
+        console.log("Jquery loaded")
+    });
 
 </script>
